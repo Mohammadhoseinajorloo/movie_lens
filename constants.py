f2d498bdf52a1738da4310c59f3360fc21a8ca14
mohammad <mohammadhoseinajorloo76@gmail.com>
2024-04-23 20:22:26 +0330

    feat(constants-file): add constants
    add constants for path file csv

---
+++ constants.py
@@ -0,0 +1,4 @@
+LINKS_PATH = '/home/mohammad/Development/tutorial/movie-lens/data/links.csv'
+RATINGS_PATH = '/home/mohammad/Development/tutorial/movie-lens/data/ratings.csv'
+TAGS_PATH = '/home/mohammad/Development/tutorial/movie-lens/data/tags.csv'
+MOVIES_PATH = '/home/mohammad/Development/tutorial/movie-lens/data/movies.csv'
---
+++ main.py
@@ -0,0 +1,10 @@
+from constants import *
+import pandas as pd 
+
+if __name__ == '__main__':
+    links_dataset = pd.read_csv(LINKS_PATH)
+    ratings_dataset = pd.read_csv(RATINGS_PATH)
+    tags_dataset = pd.read_csv(TAGS_PATH)
+    movies_dataset = pd.read_csv(MOVIES_PATH)
+
+
