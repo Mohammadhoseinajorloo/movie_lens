dd07f6a595de512abba372ac4c375939ae0df269
mohammad <mohammadhoseinajorloo76@gmail.com>
2024-04-24 20:42:40 +0330

    feat(added-Dataset-class): add dataset class

--- constants.py
+++ constants.py
@@ -1,4 +1,1 @@
-LINKS_PATH = '/home/mohammad/Development/tutorial/movie-lens/data/links.csv'
-RATINGS_PATH = '/home/mohammad/Development/tutorial/movie-lens/data/ratings.csv'
-TAGS_PATH = '/home/mohammad/Development/tutorial/movie-lens/data/tags.csv'
-MOVIES_PATH = '/home/mohammad/Development/tutorial/movie-lens/data/movies.csv'
+PATH = '/home/mohammad/Development/tutorial/movie-lens/data'
--- main.py
+++ main.py
@@ -1,13 +1,35 @@
 from constants import *
 import pandas as pd 
+import os
+
+class Dataset:
+
+    def __init__(self, path):
+        self.path = path
+        if self.__is_multi_file(self.path):
+            self.__merge_files(self.path)
+        else:
+            __file = str(os.listdir(self.path)[0])
+            __file_path = os.path.join(self.path, __file)
+            self.dataset = pd.read_csv(__file_path)
+
+    def __merge_files(self, directory):
+        pass
+
+    def __is_multi_file(self, directory):
+        count_file = len(os.listdir(directory))
+        if count_file == 0:
+            raise Exception("Empty file")
+        elif count_file != 1:
+            return True
+        return False
+
+    def __repr__(self):
+        text = " "
+        for row in self.dataset.values:
+            text += f"{row}\n"
+        return text
 
 if __name__ == '__main__':
-    links_dataset = pd.read_csv(LINKS_PATH)
-    ratings_dataset = pd.read_csv(RATINGS_PATH)
-    tags_dataset = pd.read_csv(TAGS_PATH)
-    movies_dataset = pd.read_csv(MOVIES_PATH)
-
-
-
-
-
+    dataset = Dataset(PATH)
+    print(dataset)
