00f5accecec01a7f47d248c4811081cdf6d8aa9c
mohammad <mohammadhoseinajorloo76@gmail.com>
2024-05-08 23:56:46 +0330

    feat(main): function
    clean main file

--- main.py
+++ main.py
@@ -1,14 +1,30 @@
 from dataset.reader import Reader
 from preprocessing import Preprocessing
 
-def content_base_approch(preper_df):
-    pass
+def read_data(filenames):
+    movies = Reader(filenames[0]).reader().loc[:, ['movieId', 'genres']]
+    tags = Reader(filenames[1]).reader().loc[:,['userId', 'tag']]
+    ratings = Reader(filenames[2]).reader().loc[:, 'rating']
+    return list(movies, tags, ratings)
+
+def preprocessing(dataframes):
+    preper_df = Preprocessing(dataframes)
+    preper_df = preper_df.merging()
+    return preper_df
+
+
+def content_base_approch():
+    filenames = ['links.csv', 'movies.csv', 'ratings.csv']
+    dataframes = read_data(filenames)
+    preper_df = preprocessing(dataframes)
+    return preper_df
+
+def main():
+    preper_df = content_base_approch()
+    print(preper_df)
+
 
 if __name__ == "__main__":
-    movies = Reader('movies.csv').reader().loc[:, ['movieId', 'genres']]
-    tags = Reader('tags.csv').reader().loc[:,['userId', 'tag']]
-    ratings = Reader('ratings.csv').reader().loc[:, 'rating']
-    preper_df = Preprocessing([movies, tags, ratings]).merging()
-    print(preper_df)
+    main()
 
 
