80b95e255285e4880ee92b353b5c1a5b858abec6
mohammad <mohammadhoseinajorloo76@gmail.com>
2024-05-13 00:00:52 +0330

    feat(content_table): preprocessing
    add function for convert content column to dataframe

--- main.py
+++ main.py
@@ -5,21 +5,22 @@ def read_data(filenames):
     movies = Reader(filenames[0]).reader()
     tags = Reader(filenames[1]).reader()
     ratings = Reader(filenames[2]).reader()
-    return list((movies, tags, ratings))
+    links = Reader(filenames[3]).reader()
+    return movies, tags, ratings, links
 
-def preprocessing(dataframes):
-    preper_df = Preprocessing(dataframes)
+def preprocessing(dataframe):
+    preper_df = Preprocessing(dataframe)
     preper_df.drop_duplicates()
     preper_df.check_missing_data()
-    print(preper_df.find_outliers_IQR(1.5))
+    preper_df.find_outliers_IQR(1.5)
     return preper_df
 
 
 def content_base_approch():
-    filenames = ['movies.csv', 'tags.csv', 'ratings.csv']
-    dataframes = read_data(filenames)
-    preper_df = preprocessing(dataframes)
-    return preper_df
+    filenames = ['movies.csv', 'tags.csv', 'ratings.csv', 'links.csv']
+    movies, tags, ratings, links = read_data(filenames)
+    movies = preprocessing(movies)
+    return movies 
 
 def main():
     preper_df = content_base_approch()
--- preprocessing/preprocessing.py
+++ preprocessing/preprocessing.py
@@ -1,62 +1,91 @@
 import pandas as pd
+import numpy as np
 
 class Preprocessing:
 
-    def __init__(self, dfs):
-        self.dfs = dfs
+    def __init__(self, df):
+        self.df = df
 
     def drop_duplicates(self, columns=None):
-        for df in self.dfs:
-            if columns == None:
-                df.drop_duplicates(inplace=True)
-            else:
-                df.drop_duplicates(subset=columns, inplace=True)
+        if columns == None:
+            self.df.drop_duplicates(inplace=True)
+        else:
+            self.df.drop_duplicates(subset=columns, inplace=True)
 
-    def check_missing_data(self):
-        for df in self.dfs:
-            proportion_null_rows = 100*(round(df.isnull().sum()/df.count(), 2))
-            if proportion_null_rows.all() <= 5:
-                print(f"There are {df.isnull().sum()} rows with a null value. All of them are erased!")
-                df.dropna()
-            else:
+    def check_missing_data(self, verbos=False):
+        proportion_null_rows = 100*(round(self.df.isnull().sum()/self.df.count(), 2))
+        if proportion_null_rows.all() <= 5:
+            if verbos:
+                print(f"There are {self.df.isnull().sum().sum()} rows with a null value. All of them are erased!")
+            self.df.dropna()
+        else:
+            if verbos == True:
                 print("Too many null values, we need to check columns by columns further.")
-                if df.isnull().sum().sum() > 0:
+            if df.isnull().sum().sum() > 0:
+                if verbos:
                     print("\nProportion of missing values by column")
-                    value = 100*(round(df.isnull().sum()/df.count(), 2))
-                    print(value)
-                    self._dealing_missing_data(df)
-                else:
-                    print('No missing values detected')
+                value = 100*(round(self.df.isnull().sum()/df.count(), 2))
+                print(value)
+                self._dealing_missing_data(df)
+            else:
+                print('No missing values detected')
 
-    def _dealing_missing_data(self):
-        for df in self.dfs:
-            value = 100*(round(df.isnull().sum()/df.count(), 2))
-            to_delete = []
-            to_impute = []
-            to_check = []
-            for name , proportion in value.items():
-                if int(proportion) == 0:
-                    continue
-                elif int(proprtion) <= 10:
-                    to_impute.append(name)
-                    df.fillna(df[name].median())
-                else:
-                    to_check.append(name)
-            print(f"\nThe missing values in {to_impute} have been replaced by the median.")
-            print(f"The columns {to_check} should be further understood")
 
     def find_outliers_IQR(self, coefficient):
         outlier_indices = []
-        for df in self.dfs:
-            df = df.select_dtypes(include=['number'])
-            for column in df.columns:
-                Q1 = df[column].quantile(0.25)
-                Q3 = df[column].quantile(0.75)
-                IQR = Q3 - Q1
-                lower_bound = Q1 - coefficient * IQR 
-                upper_bound = Q3 - coefficient * IQR
-                outlier_list_col = df [(df[column] < lower_bound) | df[column] > upper_bound].index
-                outlier_indices.extend(outlier_list_col)
-            outlier_indices = list(set(outlier_indices))
-            df.iloc[outlier_indices]
+        df = self.df.select_dtypes(include=['number'])
+        for column in df.columns:
+            Q1 = df[column].quantile(0.25)
+            Q3 = df[column].quantile(0.75)
+            IQR = Q3 - Q1
+            lower_bound = Q1 - coefficient * IQR 
+            upper_bound = Q3 - coefficient * IQR
+            outlier_list_col = df [(df[column] < lower_bound) | df[column] > upper_bound].index
+            outlier_indices.extend(outlier_list_col)
+        outlier_indices = list(set(outlier_indices))
+        df.iloc[outlier_indices]
         return outlier_indices
+
+
+    def content_table(self, series: pd.Series) -> pd.DataFrame:
+        '''
+        This function vlaue into a series convert to column
+        header and index per fill in eche columns.
+        input:
+            series -> content series whith need convert to header columns
+        output:
+            dataframe -> finaly dataframe with seting values
+        '''
+        content_set = slef._content_set(series)
+        row = ds.shape[0]
+        column = len(content_set)
+        shape = (row, column)
+        zero_array = np.zeros(shape)
+        df_zeros = pd.DataFrame(zero_array, columns=content_set)
+        for i, value in enumerate(ds):
+            if sep in ['-', '|', '/', ':']:
+                value = value.split(sep)
+                for item in value:
+                    for c in df_zeros.columns:
+                        if item == c:
+                            df_zeros.loc[i,c] = 1
+                        df_zeros.loc[i,c] = 0
+            raise Extention(f'This seprator {sep} invlide!!!')
+        return df_zeros
+
+
+    def _dealing_missing_data(self):
+        value = 100*(round(self.df.isnull().sum()/df.count(), 2))
+        to_delete = []
+        to_impute = []
+        to_check = []
+        for name , proportion in value.items():
+            if int(proportion) == 0:
+                continue
+            elif int(proprtion) <= 10:
+                to_impute.append(name)
+                df.fillna(df[name].median())
+            else:
+                to_check.append(name)
+        print(f"\nThe missing values in {to_impute} have been replaced by the median.")
+        print(f"The columns {to_check} should be further understood")
